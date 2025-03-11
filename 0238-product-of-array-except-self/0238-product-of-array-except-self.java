f0c42062c53f603da5063d8bf9a0e8ad37d67e87
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2025-03-11 20:00:05 +0700

    Time: 2 ms (87.89%), Space: 55.1 MB (97.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,32 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int[] result = new int[nums.length];
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            int ridx = nums.length - 1 -i;
-            if(i == 0){
-                left[i] = nums[i];
-                right[ridx] = nums[ridx];
-                continue;
-            }
-
-            left[i] = left[i-1] * nums[i];
-            right[ridx] = right[ridx+1] * nums[ridx];
-
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = 1;
         }
-        for(int i = 0; i < nums.length; i++){
-            if(i == 0){
-                result[i] = right[i+1];
-                continue;
-            }
-            if(i == nums.length - 1){
-                result[i] = left[i-1];
-                continue;
-            }
-            result[i] = left[i-1] * right[i+1];
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] *= left;
+            left *= nums[i];
         }
-        return result;
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;        
     }
 }
