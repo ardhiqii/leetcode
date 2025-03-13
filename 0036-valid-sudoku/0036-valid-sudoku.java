341cfb2609f3096e5a2a8a672840f9c30f84d2e9
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2025-03-13 16:39:24 +0700

    Time: 38 ms (5.72%), Space: 45.2 MB (9.7%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character> grid1 = new HashSet<>();
+        HashSet<Character> grid2 = new HashSet<>();
+        HashSet<Character> grid3 = new HashSet<>();
+        for(int i = 0; i < 9; i++){
+            HashSet<Character> row = new HashSet<>();
+            HashSet<Character> col = new HashSet<>();
+            if(i%3 == 0 && i> 0){
+                System.out.println();
+                grid1.clear();
+                grid2.clear();
+                grid3.clear();
+            }
+            for(int j = 0; j < 9; j++){
+                char currRow = board[i][j];
+                char currCol = board[j][i];
+                if(currRow != '.'){
+                    if(!row.add(currRow)) return false;
+                    if(j <=2){
+                        if(!grid1.add(currRow)) return false;
+                    }else if(j<=5){
+                        if(!grid2.add(currRow)) return false;
+                        System.out.printf("| %d %d %c |",i,j,currRow);
+                    }else{
+                        if(!grid3.add(currRow)) return false;
+                    }
+
+
+                    
+                    // if(j < 6 && j >=3){
+                    //     if(!grid2.add(currRow)) return false;
+                    // }
+                    // if(j < 9 && j >=6){
+                    //     if(!grid3.add(currRow)) return false;
+                    // }
+                }
+                if(currCol != '.'){
+                    if(!col.add(currCol)) return false;
+                }
+
+            }
+            
+        }
+        return true;
+    }
+}
