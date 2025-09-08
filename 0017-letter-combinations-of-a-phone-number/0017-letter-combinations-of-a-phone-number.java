e6daceb6a9c85a63939269f7f18efa25fc77c1fe
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2025-09-08 12:58:47 +0700

    Time: 0 ms (100%), Space: 42.4 MB (38.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,33 +1,45 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        ArrayList<String> res = new ArrayList<>();
-        if(digits.length() == 0) return res;
-        HashMap<Character,String> hm = new HashMap<>();
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
-        bete(0,0,digits,hm,"",res);
+        List<String> res = new ArrayList<>();
+        if (digits.length() == 0) return res;
+
+        HashMap<Character, String> hm = new HashMap<>();
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+
+        bete(0, 0, digits, hm, new StringBuilder(), res);
         return res;
     }
-    private void bete(int idx, int idx_let, String digits,HashMap<Character,String> hm,String curr,ArrayList<String> res){
-        if(curr.length() == digits.length()){
-            res.add(curr);
+
+    private void bete(int idx, int idx_let, String digits,
+                      HashMap<Character, String> hm,
+                      StringBuilder curr, List<String> res) {
+
+        if (curr.length() == digits.length()) {
+            res.add(curr.toString());
             return;
         }
-        if(idx >= digits.length()){
-            return;
-        };
-        String letter = hm.get(digits.charAt(idx)); 
-        if(idx_let >= letter.length()){
+        if (idx >= digits.length()) {
             return;
         }
-        String newCurr = curr + letter.charAt(idx_let);
-        bete(idx+1,0,digits,hm,newCurr,res);
-        bete(idx,idx_let+1,digits,hm,curr,res);
+
+        String letter = hm.get(digits.charAt(idx));
+        if (idx_let >= letter.length()) {
+            return;
+        }
+
+        // choose this letter
+        curr.append(letter.charAt(idx_let));
+        bete(idx + 1, 0, digits, hm, curr, res);
+        curr.deleteCharAt(curr.length() - 1); // backtrack
+
+        // try next letter of this digit
+        bete(idx, idx_let + 1, digits, hm, curr, res);
     }
 }
