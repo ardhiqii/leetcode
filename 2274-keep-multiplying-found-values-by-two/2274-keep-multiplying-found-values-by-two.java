f2951e620abcdae144dcc6a7af6f49799475e813
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2025-11-19 14:13:22 +0700

    Time: 3 ms (69.35%), Space: 46.2 MB (15.22%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int value = original;
+        for(int i = 0; i < nums.length; i++){
+            arr.add(nums[i]);
+        }
+        while(arr.contains(value)){
+            value*=2;
+        }
+        return value;
+    }
+}
