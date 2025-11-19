09f4dd58d4e7d5cdcb2b6ba7694076345a6e07c1
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2025-11-19 14:22:49 +0700

    Time: 3 ms (69.35%), Space: 46.4 MB (7.36%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -4,6 +4,7 @@ class Solution {
         int value = original;
         for(int i = 0; i < nums.length; i++){
             arr.add(nums[i]);
+            if(nums[i] == value)value*=2;
         }
         while(arr.contains(value)){
             value*=2;
