2c00b8f4af69e94071682d5070872927209e1f60
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2026-06-04 10:30:34 +0700

    Time: 1 ms (76.68%), Space: 46.9 MB (8.23%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,20 +3,15 @@ class Solution {
         int k = 0;
         for(int i = 0; i < nums.length; i++){
             int j = i+1;
-            
             while(j < nums.length && nums[i] == nums[j]){
                 j++;
             }
             k++;
             if(k < nums.length && j< nums.length){
-                // System.out.printf("%d | %d\n", nums[j],j);
                 nums[k]=nums[j];
             }
             i=j-1;
-
-            
         }
-        System.out.println(k);
         return k;
     }
 }
