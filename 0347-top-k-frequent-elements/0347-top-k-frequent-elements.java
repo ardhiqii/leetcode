74550d53a055c70133f9e12033a3f50a4605a036
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2026-06-18 14:35:13 +0700

    Time: 20 ms (8.64%), Space: 47.9 MB (20.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int n : nums){
-            hm.put(n,hm.getOrDefault(n,0)+1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i =0; i < nums.length;i++){
+            int val = hm.getOrDefault(nums[i],0)+1;
+            hm.put(nums[i],val);
+        }
+        int[] ans = new int[k];
+        int i = 0;
+
+        for(Map.Entry<Integer,Integer> m : hm.entrySet()){
+            pq.add(m.getValue());
+            if(pq.size() > k) pq.poll();
+        }
+        for(Map.Entry<Integer,Integer> m : hm.entrySet()){
+            if(pq.contains(m.getValue())){
+                ans[i] = m.getKey();
+                i++;
+            }
         }
 
-        ArrayList<Integer>[] bucket = new ArrayList[nums.length + 1];
-        for(int key: hm.keySet()){
-            int freq = hm.get(key);
-            if(bucket[freq] == null){
-                bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(key);
-        }
-
-        int[] result = new int[k];
-        int idx = 0;
-        for(int i = bucket.length - 1; i>=0; i--){
-            if(bucket[i] != null){
-                for(int num : bucket[i]){
-                    result[idx++] = num;
-                    if(idx == k) return result;
-                }   
-            }
-            
-        }
-        return result;
+        return ans;
     }
 }
